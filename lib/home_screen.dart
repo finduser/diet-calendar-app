ea9dc67b04cb4e08d9e2509ad3d58612ee1edd95
finduser <dudziecdamiande@gmail.com>
2022-11-27 19:13:46 +0100

    add: simple layout

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const daysOfWeek = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ];
+
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(child: Text("Button"), onPressed: () => Navigator.of(context).pushNamed('/test'),)
-      ),
+      body: Row(children: [
+        Container(
+          width: MediaQuery.of(context).size.width - 200,
+          height: MediaQuery.of(context).size.height - 200,
+          margin: const EdgeInsets.all(100),
+          color: Colors.amber,
+          child: Column(
+              children: daysOfWeek.map(
+            (element) {
+              return Text(element);
+            },
+          ).toList()),
+        )
+      ]),
     );
   }
 }
